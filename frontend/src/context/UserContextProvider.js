bb3c34fd211df1c00880df40e234a8a4a355da58
Kraae <117115729+Kraae@users.noreply.github.com>
2024-02-01 03:07:21 -0500

    Update UserContextProvider.js

--- frontend/src/context/UserContextProvider.js
+++ frontend/src/context/UserContextProvider.js
@@ -33,7 +33,7 @@ function UserContextProvider({ children }) {
         try {
             let token = await DnDApi.register(signUpData)
             setToken(token)
-            navigate(`\`);
+            navigate(`/`);
             return { success: true }
         } catch (err) {
             console.error('signup failed', err)
